b66a902cfaf063bad088de19fc2eee7b9b07bd48
appwithash <ashutoshpandey.developer@gmail.com>
2023-06-02 20:02:18 +0530

    Initial Commit

---
+++ ProjectDemo/ProjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDemoApp.swift
+//  ProjectDemo
+//
+//  Created by ashutosh on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
